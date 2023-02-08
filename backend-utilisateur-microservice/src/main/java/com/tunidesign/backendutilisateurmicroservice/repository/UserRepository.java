cb43f7f1a3e49deedbbc8633f73ceb2fa4aa812c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-08 12:45:53 +0100

    repositories

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tunidesign.backendutilisateurmicroservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tunidesign.backendutilisateurmicroservice.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
