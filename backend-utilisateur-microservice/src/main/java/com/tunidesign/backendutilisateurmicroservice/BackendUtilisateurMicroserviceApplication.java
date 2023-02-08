b37a707bc7a1cb86921d64745231bb4bd86b722c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-08 10:14:31 +0100

    utilisateur microservice

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/BackendUtilisateurMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tunidesign.backendutilisateurmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUtilisateurMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUtilisateurMicroserviceApplication.class, args);
+	}
+
+}
