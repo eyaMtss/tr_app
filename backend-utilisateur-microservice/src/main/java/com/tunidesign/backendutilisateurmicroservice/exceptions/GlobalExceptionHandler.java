c4cab8379dc676f27fe2fe6a8b81d19c3076c55e
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-08 17:32:57 +0100

    GlobalExceptionHandler

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.tunidesign.backendutilisateurmicroservice.exceptions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.http.HttpHeaders;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<String> AttributeLengthException(UserNotFoundException e) {
+		return new ResponseEntity<String>("ERROR: " + e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ErrorMessage> customeException(CustomException ex, WebRequest request,
+			HttpServletRequest httpRequest) {
+		ErrorMessage errors = new ErrorMessage();
+		errors.setMessage(ex.getMessage());
+		errors.setStackTrace(ex.getStackTrace().toString());
+		return new ResponseEntity<>(errors, HttpStatus.NOT_ACCEPTABLE);
+	}
+
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		Map<String, Object> body = new LinkedHashMap<String, Object>();
+		body.put("status", status.value());
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
+				.collect(Collectors.toList());
+		body.put("errors", errors);
+		return new ResponseEntity<>(body, headers, status);
+	}
+}
