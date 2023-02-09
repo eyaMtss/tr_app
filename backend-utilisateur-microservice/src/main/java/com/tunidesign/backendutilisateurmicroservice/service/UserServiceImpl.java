65ff940826b9938220f6e1f45e18264e936b1c8b
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 14:06:22 +0100

    implements add/get methods

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/service/UserServiceImpl.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/service/UserServiceImpl.java
@@ -10,19 +10,68 @@ import com.tunidesign.backendutilisateurmicroservice.DTO.UserRequestDTO;
 import com.tunidesign.backendutilisateurmicroservice.DTO.UserResponseDTO;
 import com.tunidesign.backendutilisateurmicroservice.mapper.UserMapperImpl;
 import com.tunidesign.backendutilisateurmicroservice.model.entity.User;
+import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Role;
 import com.tunidesign.backendutilisateurmicroservice.repository.UserRepository;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	private UserMapperImpl userMapper;
+	private UserMapperImpl userMapper = new UserMapperImpl();
+
 	@Override
 	public UserResponseDTO addUser(UserRequestDTO userRequestDTO) {
 		return userMapper.userToUserResponseDTO(userRepository.save(userMapper.userRequestDTOToUser(userRequestDTO)));
 	}
 
+	@Override
+	public UserResponseDTO addClient(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.CLEINT);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addTA(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.TA);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addDriver(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.DRIVER);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addExpert(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.EXPERT);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addCompanyAdmin(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.COMPANY_ADMIN);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addAgencyAdmin(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.AGENCY_ADMIN);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addInsuranceAdmin(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.INSURANCE_ADMIN);
+		return addUser(userRequestDTO);
+	}
+
+	@Override
+	public UserResponseDTO addPrestataire(UserRequestDTO userRequestDTO) {
+		userRequestDTO.setRole(Role.PRESTATAIRE);
+		return addUser(userRequestDTO);
+	}
 
 	@Override
 	public UserResponseDTO uploadPicture(PictureRequestDTO pictureRequestDTO) {
@@ -32,21 +81,19 @@ public class UserServiceImpl implements UserService{
 		existingUser.setPictureByte(pictureRequestDTO.getPictureByte());
 		return userMapper.userToUserResponseDTO(userRepository.save(existingUser));
 	}
-	
+
 	@Override
 	public UserResponseDTO updateUser(UserRequestDTO userRequestDTO) {
-		//User existingUser = userRepository.findById(userRequestDTO.getUserId()).get();
+		// User existingUser =
+		// userRepository.findById(userRequestDTO.getUserId()).get();
 		User user = userMapper.userRequestDTOToUser(userRequestDTO);
-		User savedUser= userRepository.save(user);
+		User savedUser = userRepository.save(user);
 		return userMapper.userToUserResponseDTO(savedUser);
 	}
 
 	@Override
 	public List<UserResponseDTO> getUsers() {
-		return userRepository.findAll()
-				.stream()
-				.map(user -> userMapper.userToUserResponseDTO(user))
-				.toList();
+		return userRepository.findAll().stream().map(user -> userMapper.userToUserResponseDTO(user)).toList();
 	}
 
 	@Override
@@ -57,13 +104,54 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public void deleteUser(Long userId) {
 		userRepository.deleteById(userId);
-		
+
 	}
 
 	@Override
-	public Boolean isExist(Long id) {
-		return userRepository.findById(id).isPresent();
+	public Boolean isExist(Long userId) {
+		return userRepository.findById(userId).isPresent();
 	}
 
+	@Override
+	public List<UserResponseDTO> getClients() {
+		return userRepository.findByRole(Role.CLEINT).stream().map(user -> userMapper.userToUserResponseDTO(user))
+				.toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getDrivers(Long companyId) {
+		return userRepository.findCompanyEmployeesByRole(companyId, Role.DRIVER).stream()
+				.map(user -> userMapper.userToUserResponseDTO(user)).toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getTAs(Long companyId) {
+		return userRepository.findCompanyEmployeesByRole(companyId, Role.TA).stream()
+				.map(user -> userMapper.userToUserResponseDTO(user)).toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getExperts(Long companyId) {
+		return userRepository.findCompanyEmployeesByRole(companyId, Role.EXPERT).stream()
+				.map(user -> userMapper.userToUserResponseDTO(user)).toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getCompanyEmployees(Long companyId) {
+		return userRepository.findByCompanyId(companyId).stream().map(user -> userMapper.userToUserResponseDTO(user))
+				.toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getAgencyEmployees(Long agencyId) {
+		return userRepository.findByAgencyId(agencyId).stream().map(user -> userMapper.userToUserResponseDTO(user))
+				.toList();
+	}
+
+	@Override
+	public List<UserResponseDTO> getInsuranceEmployees(Long insuranceId) {
+		return userRepository.findByInsuranceCompanyId(insuranceId).stream()
+				.map(user -> userMapper.userToUserResponseDTO(user)).toList();
+	}
 
 }
