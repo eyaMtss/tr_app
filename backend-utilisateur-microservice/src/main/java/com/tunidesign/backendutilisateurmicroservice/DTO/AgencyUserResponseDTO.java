22fd2ec7ba2de8950aaaf9495bbfe5dc56176b0c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 17:23:18 +0100

    Agency DTO

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/DTO/AgencyUserResponseDTO.java
@@ -0,0 +1,46 @@
+package com.tunidesign.backendutilisateurmicroservice.DTO;
+
+import java.util.Date;
+
+import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Gender;
+import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Status;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgencyUserResponseDTO {
+	private Long userId;
+	private String firstname;
+	private String lastname;
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	private Date birthdate;
+
+	// Address
+	private String country;
+	private String governorate;
+	private String city;
+	private Integer zipCode;
+	private Integer homeCode;
+
+	// Contact
+	@Email
+	private String email;
+	private Long phoneNumber;
+
+	// employee
+	private String matriculeFiscale;
+	@Enumerated(EnumType.STRING)
+	private Status status;
+
+	private Long agencyId;
+}
