ce599428f12d28a707714dfa419f9cccf0b71dfe
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 21:09:28 +0100

    findAgencyEmployeesByRole && findInsuranceEmployeesByRole

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/repository/UserRepository.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.tunidesign.backendutilisateurmicroservice.DTO.UserResponseDTO;
 import com.tunidesign.backendutilisateurmicroservice.model.entity.User;
 import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Role;
 
@@ -20,4 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	@Query(value = "SELECT u FROM Users u WHERE u.companyId = :companyId AND u.role = :role", nativeQuery = true)
 	List<User> findCompanyEmployeesByRole(@Param("companyId") Long companyId, @Param("role") Role role);
+	
+	@Query(value = "SELECT u FROM Users u WHERE u.agencyId = :agencyId AND u.role = :role", nativeQuery = true)
+	List<User> findAgencyEmployeesByRole(@Param("agencyId") Long agencyId, @Param("role") Role role);
+	
+	@Query(value = "SELECT u FROM Users u WHERE u.insuranceId = :insuranceId AND u.role = :role", nativeQuery = true)
+	List<User> findInsuranceEmployeesByRole(@Param("insuranceId") Long insuranceId, @Param("role") Role role);
 }
