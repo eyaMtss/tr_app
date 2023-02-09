4cd006c3e21cac0686f603a6ecb5816b6e8df6f7
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 17:26:37 +0100

    Handle Exception

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/exceptions/GlobalExceptionHandler.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tunidesign.backendutilisateurmicroservice.exceptions;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,8 +8,10 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -43,4 +46,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		body.put("errors", errors);
 		return new ResponseEntity<>(body, headers, status);
 	}
+	
+	//@ResponseStatus(HttpStatus.BAD_REQUEST)
+	//@ExceptionHandler(MethodArgumentNotValidException.class)
+//	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+//		Map<String, String> errors = new HashMap<>();
+//		ex.getBindingResult().getAllErrors().forEach((error) -> {
+//			String fieldName = ((FieldError) error).getField();
+//			String errorMessage = error.getDefaultMessage();
+//			errors.put(fieldName, errorMessage);
+//		});
+//		return errors;
+//	}
 }
