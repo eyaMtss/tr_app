b5fd206d449766b89186d9d058cad119a2536a66
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 14:05:26 +0100

    Role enumeration

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/model/enumeration/Role.java
@@ -0,0 +1,6 @@
+package com.tunidesign.backendutilisateurmicroservice.model.enumeration;
+
+public enum Role {
+	CLEINT, SUPER_ADMIN, COMPANY_ADMIN, TA, DRIVER, AGENCY_ADMIN, INSURANCE_ADMIN, 
+	EXPERT, PRESTATAIRE
+}
