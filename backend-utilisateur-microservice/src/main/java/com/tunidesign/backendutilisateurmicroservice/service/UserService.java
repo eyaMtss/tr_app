cedc2f32b8366178c8f6e21530c710519b1627d8
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 14:06:06 +0100

    add / get users by role

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/service/UserService.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/service/UserService.java
@@ -8,17 +8,32 @@ import java.util.List;
 
 public interface UserService {
 	UserResponseDTO addUser(UserRequestDTO userRequestDTO);
+	UserResponseDTO addClient(UserRequestDTO userRequestDTO);
+	UserResponseDTO addTA(UserRequestDTO userRequestDTO);
+	UserResponseDTO addDriver(UserRequestDTO userRequestDTO);
+	UserResponseDTO addCompanyAdmin(UserRequestDTO userRequestDTO);
+	UserResponseDTO addAgencyAdmin(UserRequestDTO userRequestDTO);
+	UserResponseDTO addInsuranceAdmin(UserRequestDTO userRequestDTO);
+	UserResponseDTO addExpert(UserRequestDTO userRequestDTO);
+	UserResponseDTO addPrestataire(UserRequestDTO userRequestDTO);
 	
 	UserResponseDTO uploadPicture(PictureRequestDTO pictureRequestDTO);
 
 	UserResponseDTO updateUser(UserRequestDTO UserRequestDTO);
 
+	List<UserResponseDTO> getClients();
+	List<UserResponseDTO> getDrivers(Long companyId);
+	List<UserResponseDTO> getTAs(Long companyId);
+	List<UserResponseDTO> getExperts(Long companyId);
+	List<UserResponseDTO> getCompanyEmployees(Long companyId); 
+	List<UserResponseDTO> getAgencyEmployees(Long agencyId); 
+	List<UserResponseDTO> getInsuranceEmployees(Long insuranceId);
 	List<UserResponseDTO> getUsers();
 
 	UserResponseDTO getUser(Long userId);
 
-	void deleteUser(Long shiftId);
+	void deleteUser(Long userId);
 	
-	Boolean isExist(Long shiftId);
+	Boolean isExist(Long userId);
 
 }
