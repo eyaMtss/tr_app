f735435a55cb0da5d4df60b1a85994c78118116f
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 21:59:25 +0100

    add status

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/model/enumeration/Status.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/model/enumeration/Status.java
@@ -1,6 +1,5 @@
 package com.tunidesign.backendutilisateurmicroservice.model.enumeration;
 
-// HORS_LIGNE :0
 public enum Status {
-	HORS_LIGNE, DISPONIBLE, AT_WORK
+	HORS_LIGNE, DISPONIBLE, AT_WORK, RESIGNED, FIRED
 }
