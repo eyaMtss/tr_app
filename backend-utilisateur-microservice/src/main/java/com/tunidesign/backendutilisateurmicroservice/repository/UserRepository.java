8e61f8552522316f63263dced1545a43a15c0f36
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 14:06:47 +0100

    add methods

--- backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/repository/UserRepository.java
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/repository/UserRepository.java
@@ -1,11 +1,23 @@
 package com.tunidesign.backendutilisateurmicroservice.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.tunidesign.backendutilisateurmicroservice.DTO.UserResponseDTO;
 import com.tunidesign.backendutilisateurmicroservice.model.entity.User;
+import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Role;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	List<User> findByCompanyId(Long companyId);
+	List<User> findByAgencyId(Long companyId);
+	List<User> findByInsuranceCompanyId(Long companyId);
+	
+	List<User> findByRole(Role role);
+	
+	@Query(value = "SELECT u FROM Users u WHERE u.companyId = :companyId AND u.role = :role", nativeQuery = true)
+	List<User> findCompanyEmployeesByRole(@Param("companyId") Long companyId, @Param("role") Role role);
 }
