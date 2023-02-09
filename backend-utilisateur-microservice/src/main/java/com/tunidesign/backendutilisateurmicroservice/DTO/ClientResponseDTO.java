e14cf755f26485bd810b19ecba127cbba5894256
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-09 17:22:59 +0100

    Insurance DTO

---
+++ backend-utilisateur-microservice/src/main/java/com/tunidesign/backendutilisateurmicroservice/DTO/ClientResponseDTO.java
@@ -0,0 +1,37 @@
+package com.tunidesign.backendutilisateurmicroservice.DTO;
+
+import java.util.Date;
+
+import com.tunidesign.backendutilisateurmicroservice.model.enumeration.Gender;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientResponseDTO {
+	private Long userId;
+	private String firstname;
+	private String lastname;
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	private Date birthdate;
+
+	// Address
+	private String country;
+	private String governorate;
+	private String city;
+	private Integer zipCode;
+	private Integer homeCode;
+
+	// Contact
+	@Email
+	private String email;
+	private Long phoneNumber;
+}
