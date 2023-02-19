4fb3f7979d9d8e373f583236d540714dacbe307a
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-19 12:15:00 +0100

    commit new project

---
+++ utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/UtilisateurMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tunidesign.utilisateurmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisateurMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilisateurMicroserviceApplication.class, args);
+	}
+
+}
