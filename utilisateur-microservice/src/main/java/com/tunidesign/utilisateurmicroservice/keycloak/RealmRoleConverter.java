5580bdbdb419a2e01c6268ef1a1f8b851f2eba75
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-27 17:31:05 +0100

    keycloak config

--- utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/keycloak/RealmRoleConverter.java
+++ utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/keycloak/RealmRoleConverter.java
@@ -1,20 +1,21 @@
-package com.tunidesign.utilisateurmicroservice.model.util;
+package com.tunidesign.utilisateurmicroservice.keycloak;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RealmRoleConverter  implements Converter<Jwt, Collection<GrantedAuthority>> {
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
-        final Map<String, List<String>> realmAccess = (Map<String, List<String>>) jwt.getClaims().get("realm_access");
-        return realmAccess.get("roles")
+        final Map<String, List<String>> realmAccess = new HashMap<>();
+        realmAccess.put("role", Arrays.asList(jwt.getClaims().get("role").toString()));
+        //= (Map<String, List<String>>) jwt.getClaims().get("role");
+        System.out.println(realmAccess);
+        return realmAccess.get("role")
                 .stream()
                 .map(roleName -> "ROLE_" + roleName) // prefix required by Spring Security for roles.
                 .map(SimpleGrantedAuthority::new)
