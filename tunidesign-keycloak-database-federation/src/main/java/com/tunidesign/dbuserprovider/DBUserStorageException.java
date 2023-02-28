776d18c984d85f24927d60070d336c54a4c07c9c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-02-28 16:10:22 +0100

    exception

---
+++ tunidesign-keycloak-database-federation/src/main/java/com/tunidesign/dbuserprovider/DBUserStorageException.java
@@ -0,0 +1,16 @@
+package com.tunidesign.dbuserprovider;
+
+public class DBUserStorageException extends RuntimeException {
+
+    public DBUserStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DBUserStorageException(Throwable cause) {
+        super(cause);
+    }
+
+    public DBUserStorageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
