fb7d72827eab622421111aa700e00bc229bff15f
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 18:03:28 +0100

    keycloak

--- parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/keycloak/RealmRoleConverter.java
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/keycloak/RealmRoleConverter.java
@@ -1,4 +1,4 @@
-package com.tunidesign.utilisateurmicroservice.keycloak;
+package com.tunidesign.parkingmicroservice.keycloak;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
--- parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/keycloak/ResourceServerConfig.java
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/keycloak/ResourceServerConfig.java
@@ -1,4 +1,4 @@
-package com.tunidesign.utilisateurmicroservice.keycloak;
+package com.tunidesign.parkingmicroservice.keycloak;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,6 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
