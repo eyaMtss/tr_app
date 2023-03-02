ae98318c0c78783458507278d0bd591074521e4f
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 12:31:45 +0100

    parking microservice project

---
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/ParkingMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.tunidesign.parkingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ParkingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingMicroserviceApplication.class, args);
+	}
+
+}
