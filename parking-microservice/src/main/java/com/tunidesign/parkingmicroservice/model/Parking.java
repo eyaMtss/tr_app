40f048840222b5a3b0bc7da52dff77c7682295e6
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 12:59:40 +0100

    + builder

--- parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/model/Parking.java
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/model/Parking.java
@@ -2,6 +2,7 @@ package com.tunidesign.parkingmicroservice.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Parking {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
