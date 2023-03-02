ba5ca9ff25703cf762e8b7938e2e9bf195010891
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 13:26:37 +0100

    add findAllByGarageId

--- parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/repository/ParkingRepository.java
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/repository/ParkingRepository.java
@@ -4,6 +4,9 @@ import com.tunidesign.parkingmicroservice.model.Parking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParkingRepository extends JpaRepository<Parking, Long> {
+    List<Parking> findAllByGarageId(Long garageId);
 }
