b42ace6f6436a25d83ff8cdb4888650a97d063bb
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 13:26:01 +0100

    add methods

--- parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/service/ParkingService.java
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/service/ParkingService.java
@@ -1,5 +1,17 @@
 package com.tunidesign.parkingmicroservice.service;
 
-public interface ParkingService {
+import com.tunidesign.parkingmicroservice.DTO.ParkingRequestDTO;
+import com.tunidesign.parkingmicroservice.DTO.ParkingResponseDTO;
+import com.tunidesign.parkingmicroservice.model.Parking;
 
+import java.util.List;
+
+public interface ParkingService {
+    public ParkingResponseDTO addParking(ParkingRequestDTO parkingRequestDTO);
+    public ParkingResponseDTO updateParking(Long parkingId, ParkingRequestDTO parkingRequestDTO);
+    public List<ParkingResponseDTO> getParkings();
+    public List<ParkingResponseDTO> getParkingsByGarage(Long garageId);
+    public ParkingResponseDTO getParkingById(Long parkingId);
+    public void deleteParking(Long parkingId);
+    public Boolean isExist(Long parkingId);
 }
