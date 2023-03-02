221e89b33accbac8efe92fa5138c4a5a6d256980
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-02 12:43:45 +0100

    parking entity

---
+++ parking-microservice/src/main/java/com/tunidesign/parkingmicroservice/model/Parking.java
@@ -0,0 +1,28 @@
+package com.tunidesign.parkingmicroservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Parking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Integer capacity;
+    private Integer nbVehicle;
+    private String country;
+    private String governorate;
+    private String city;
+    private String zipCode;
+    private Double longitude;
+    private Double latitude;
+    private Long idGarage;
+
+}
