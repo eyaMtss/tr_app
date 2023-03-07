98f0345c91ab5aa214cb1fcbeedfbea335b47404
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-07 20:18:46 +0100

    isFailed + onForgotPasswordButton

--- Front-End/src/app/login-page/login-page.component.ts
+++ Front-End/src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { emailOrUsernameValidator } from '../shared/validator/email-or-username.validator';
+import { AuthService } from '../services/authentication/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,24 +11,38 @@ import { emailOrUsernameValidator } from '../shared/validator/email-or-username.
 })
 export class LoginPageComponent implements OnInit {
   loginForm: FormGroup; //form
-  constructor(private fb: FormBuilder, private router: Router) {
+  isFailed: boolean = false; // Authentication failed after login (Invalid username or password)
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
     this.loginForm = this.fb.group({
-      username: ["", Validators.required, emailOrUsernameValidator(/[a-zA-Z0-9]{3,}/)],
+      username: ["", Validators.required],// emailOrUsernameValidator(/[a-zA-Z0-9]{3,}/)],
       password: ["", Validators.required],
       rememberMe: [false]
-    })
+    });
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit()
-  {
-    console.log(this.loginForm);
-    this.router.navigate(['/order']);
-    
-    //{ queryParams: { label: this.currentLabelKey, soggetto: this.datiForm.get("soggetto")?.value, tipo: this.datiForm.get("tipo")?.value } })
-    
+  onForgotPasswordButton(){
+    this.router.navigate(['/password_reset']);
+  }
+
+  onSubmit() {
+   
+    this.authService.login(this.loginForm.controls['username'].value, this.loginForm.controls['password'].value)
+      .then(() => {
+        this.isFailed = false; 
+        // User is authenticated, navigate to home page or other protected page
+        console.log(this.loginForm);
+        console.log(this.authService.getLoggedUser())
+        console.log(this.authService.getToken())
+        this.router.navigate(['/order']);
+        //{ queryParams: { label: this.currentLabelKey, soggetto: this.datiForm.get("soggetto")?.value, tipo: this.datiForm.get("tipo")?.value } })
+        
+      })
+      .catch(() => {
+        this.isFailed = true; // Authentication failed, show error message
+      });
   }
 
 }
