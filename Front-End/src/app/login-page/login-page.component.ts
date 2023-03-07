93ad93def879b3cc34cd94883cd0dfe538c077ce
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-07 11:52:02 +0100

    emailOrUsername custom valodator

--- Front-End/src/app/login-page/login-page.component.ts
+++ Front-End/src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { emailOrUsernameValidator } from '../shared/validator/email-or-username.validator';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +12,7 @@ export class LoginPageComponent implements OnInit {
   loginForm: FormGroup; //form
   constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
-      username: ["", Validators.required],
+      username: ["", Validators.required, emailOrUsernameValidator(/[a-zA-Z0-9]{3,}/)],
       password: ["", Validators.required],
       rememberMe: [false]
     })
@@ -30,3 +31,4 @@ export class LoginPageComponent implements OnInit {
   }
 
 }
+
