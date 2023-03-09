c16e5b443f2a56a7ad4c363b74bc982ff2a5fe16
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-09 13:42:50 +0100

    isLoggedIn var

--- Front-End/src/app/shared/navbar/navbar.component.ts
+++ Front-End/src/app/shared/navbar/navbar.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(private router: Router){
+export class NavbarComponent implements OnInit{
+  @Input() isLoggedIn!: boolean;
+  constructor(private router: Router, private authService: AuthService) {
+    
+  }
+  ngOnInit(): void {
+    if (this.authService.getIsLoggedIn())
+      this.isLoggedIn = true;
+    this.isLoggedIn = false;
+    localStorage.setItem("isLoggedIn", this.isLoggedIn.toString()); // set isLoggedIn attribute in LocalStorage
 
   }
 
-  home(){
+  home() {
     this.router.navigate(['/']);
   }
 
-  signin(){
+  signin() {
     this.router.navigate(['/auth']);
   }
 
-  signup(){
+  signup() {
+    this.authService.logout();
+  }
+
+  logout(){
     this.router.navigate(['/auth/signup']);
   }
 }
