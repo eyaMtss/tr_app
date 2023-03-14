eb3fecce9418b03be787e8b2f5d9c10586c704a3
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-14 12:05:45 +0100

    dto

--- societeRemorquage-microservice/src/main/java/com/tunidesign/societeRemorquagemicroservice/DTO/SocieteRemorquageRequestDTO.java
+++ societeRemorquage-microservice/src/main/java/com/tunidesign/societeRemorquagemicroservice/DTO/SocieteRemorquageRequestDTO.java
@@ -1,5 +1,7 @@
 package com.tunidesign.societeRemorquagemicroservice.DTO;
 
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class SocieteRemorquageRequestDTO {
-    private int id;
-    private String adresse;
+    private Long id;
+    private String name;
+    private String country;
+    private String governorate;
+    private String city;
+    private Integer zipCode;
+    private Double lattitude;
+    private Double longitude;
+    private Long phone1;
+    private Long phone2;
+    private String email;
+    private String logoName;
+    private String logoType;
+    @Lob
+    //@Column(length = 1000000000, columnDefinition = "LONGBLOB")
+    private byte[] logoByte;
 }
