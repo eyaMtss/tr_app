49213b07a7ef6d44198fc6bb3ea6c1049a931ea6
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-15 01:21:19 +0100

    verifyContrat DTO

--- contrat-microservice/src/main/java/com/tunidesign/contratmicroservice/DTO/VerifyContractRequestDTO.java
+++ contrat-microservice/src/main/java/com/tunidesign/contratmicroservice/DTO/VerifyContractRequestDTO.java
@@ -1,2 +1,15 @@
-package com.tunidesign.contratmicroservice.DTO;public class VerifyContractRequestDTO {
+package com.tunidesign.contratmicroservice.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class VerifyContractRequestDTO {
+    private String numContrat;
+    private Long numChassis;
 }
