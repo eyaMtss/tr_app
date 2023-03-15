15bad2869dda659d0e536eec0b66076ce67b3f5c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-15 06:58:42 +0100

    user service

--- Front-End/src/app/services/api/user.service.ts
+++ Front-End/src/app/services/api/user.service.ts
@@ -1,17 +1,52 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from 'src/app/models/client';
+import { InsuranceAdmin } from 'src/app/models/insurance-admin';
+import { SocieteRemorquageAdmin } from 'src/app/models/societe-remorquage-admin';
+import { User } from 'src/app/models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  //userURL = 'http://localhost:8080/user/';
-
-  
+  private _URL = environment.domain + ":" + environment.port + "/users/";
   constructor(private httpClient: HttpClient) { }
 
-  /*public create(user: User): Observable<any> {
-    return this.httpClient.post<any>(this.userURL + 'create', user);
+  getAll(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this._URL + "getAll")
+  }
+
+  getById(id: number): Observable<User>{
+    return this.httpClient.get<User>(this._URL + "getById/" + id);
+  }
+
+  create(user: User): Observable<User> {
+    return this.httpClient.post<User>(this._URL + "add", user);
+  }
+
+  createClient(client: Client): Observable<Client> {
+    return this.httpClient.post<Client>(this._URL + "addClient", client);
+  }
+
+  createInsuranceAdmin(insuranceAdmin: InsuranceAdmin): Observable<User> {
+    return this.httpClient.post<User>(this._URL + "addInsuranceAdmin", insuranceAdmin);
+  }
+
+  createSocieteRemorquageAdmin(societeRemorquageAdmin: SocieteRemorquageAdmin): Observable<User> {
+    return this.httpClient.post<User>(this._URL + "addCompany", societeRemorquageAdmin);
+  }
+
+  deleteUser(id: number){
+    return this.httpClient.delete<User>(this._URL + "delete/" + id);
+  }
+ 
+  editUser(user: User): Observable<User>{
+    return this.httpClient.put<User>(this._URL + "update/", user);
+  }
+
+  /*editImage(): Observable<User>{
+    return this.httpClient.put<User>(this._URL + "update/");
   }*/
 }
