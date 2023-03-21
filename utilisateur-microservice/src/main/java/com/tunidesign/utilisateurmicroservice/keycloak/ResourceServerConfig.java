c19bbde1e0503e62ae100081aa997f45bdc7572a
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-21 01:26:10 +0100

    cleanup

--- utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/keycloak/ResourceServerConfig.java
+++ utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/keycloak/ResourceServerConfig.java
@@ -1,16 +1,26 @@
 package com.tunidesign.utilisateurmicroservice.keycloak;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +32,13 @@ public class ResourceServerConfig {
                 .csrf().disable()
                 .authorizeRequests()
                 .requestMatchers("/swagger-ui/**", "/v3/**", "/users/addClient", "/users/addCompanyAdmin",
-                        "/users/addInsuranceAdmin")
+                        "/users/addInsuranceAdmin", "/users/getAll/clients")
                     .permitAll()
                 .and()
                 .authorizeRequests()
                 .anyRequest().authenticated() // OR .access("authenticated AND hasRole('product_read')")
+                .and()
+                .httpBasic()
                 .and()
                 .oauth2ResourceServer()
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()));
