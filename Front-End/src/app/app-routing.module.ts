50aec8af1c94e482acf89ae12b77daf5fc2ef02e
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-24 10:44:23 +0100

    cleanup

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -14,29 +14,29 @@ import { AccessDeniedComponent } from './access-denied/access-denied.component';
 import { QrCodeTestComponent } from './qr-code-test/qr-code-test.component';
 import { RatingComponent } from './rating/rating.component';
 import { ProfilclientComponent } from './profilclient/profilclient.component';
+import { HomeComponent } from './home/home.component';
+import { MultiComponent } from './multi/multi.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
+  { path: 'access-denied', component: AccessDeniedComponent, canActivate: [AuthGuard] },
+  { path: 'tunidesign', component: MultiComponent },
 
   {path: 'shareInformation', component: ShareInformationComponent},
-  {path: 'access-denied', component: AccessDeniedComponent, canActivate: [AuthGuard]},
-
   {path: 'share', component: ShareInformationComponent},
-
-
   {path: 'picture', component: SendPictureComponent},
   {path: 'userInformation', component: UserInformationComponent},
   {path: 'rating', component: RatingComponent},
   {path: 'pass', component: PickUpPassComponent},
   {path: 'order', component: OrderComponent},
-
   {path: 'suivi', component: SuiviOrdreComponent},
   {path: 'shift', component: ShiftComponent, canActivate:[AuthGuard], data: { roles: ['AGENCY_ADMIN']}},
-  {path: '', redirectTo: 'auth', pathMatch: 'full'},
-
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'qrcode', component: QrCodeTestComponent},
-  { path: 'profilclient/:id', component: ProfilclientComponent}
+  { path: 'profilclient/:id', component: ProfilclientComponent},
 
+  
 ];
 
 @NgModule({
