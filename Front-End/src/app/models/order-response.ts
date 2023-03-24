0a78dab544e41ee8e332836ea9ffda13201cee18
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-24 10:40:29 +0100

    /models

---
+++ Front-End/src/app/models/order-response.ts
@@ -0,0 +1,20 @@
+export class OrderResponse {
+  telephone!:number
+  breakdownType!:string
+  breakdownDetails!:string
+  isLoaded!:string
+  orderTimeRequest!:Date
+  orderTimeAccept!:Date
+  idVehicule!:number
+  positionALong!:string
+  positionAAtt!:string
+  positionBLong!:string
+  positionBAtt!:string
+  positionCLong!:string
+  positionCAtt!:string
+  numImmatriculation!:number ;
+  numChassis!:number ;
+  typeImmatriculation!:string;
+  serie!:number;
+
+}
