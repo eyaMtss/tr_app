dc60e66594fdba4b7e8b971b42401a03811d9022
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-28 11:50:35 +0200

    cleanup

--- Front-End/src/app/signup/signup.module.ts
+++ Front-End/src/app/signup/signup.module.ts
@@ -3,20 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
-import { AddressComponent } from './address/address.component';
-import { CredentialsComponent } from './credentials/credentials.component';
 import { InformationsComponent } from './informations/informations.component';
-import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
+import { CredentialsComponent } from './credentials/credentials.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     SignupComponent,
-    AddressComponent,
     CredentialsComponent,
     InformationsComponent,
-    AddVehicleComponent
   ],
   imports: [
     CommonModule,
