0ed1f9c2eb69f7392bdcb20b232833bed29fd5b4
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-28 23:21:34 +0200

    constructor

--- Front-End/src/app/models/user.ts
+++ Front-End/src/app/models/user.ts
@@ -14,26 +14,15 @@ export class User {
     password!: string;
     confirmPassword!: string;
 
-    /*constructor(email: string, firstName: string, lastName: string, phone: number, birthdate: Date,
-        gender: string, matriculeFiscale: string, companyId: number, insuranceId: number,
-        country: string, governorate: string, city: string, zipCode: number, homeCode: number,
-        password: string, confirmPassword: string) {
-
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.birthdate = birthdate;
-        this.gender = gender;
-        this.matriculeFiscale = matriculeFiscale;
-        this.companyId = companyId;
-        this.insuranceId = insuranceId;
-        this.country = country;
-        this.governorate = governorate;
-        this.city = city;
-        this.zipCode = zipCode;
-        this.homeCode = homeCode;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-    }*/
+    public constructor();
+    public constructor(firstName: string, lastName: string, email: string, phoneNumber: number,
+        birthdate: Date, gender: string);
+    public constructor(...myarray: any[]){
+        this.firstName = myarray[1];
+        this.lastName = myarray[2];
+        this.email = myarray[3];
+        this.phoneNumber = myarray[4];
+        this.birthdate = myarray[5];
+        this.gender = myarray[6];
+    }
 }
