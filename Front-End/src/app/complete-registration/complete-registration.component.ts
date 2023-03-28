c7c7bb3902c78e894434949c88d96b2e30377bc4
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-28 11:51:54 +0200

    get address

--- Front-End/src/app/complete-registration/complete-registration.component.ts
+++ Front-End/src/app/complete-registration/complete-registration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { AuthService } from '../services/auth/auth.service';
 import { UserService } from '../services/api/user.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Vehicle } from '../models/vehicle';
 
 @Component({
   selector: 'app-complete-registration',
@@ -9,8 +11,27 @@ import { UserService } from '../services/api/user.service';
   styleUrls: ['./complete-registration.component.css']
 })
 export class CompleteRegistrationComponent implements OnInit {
-  constructor(private keycloakService: KeycloakService, private authService: AuthService, private userService: UserService) { }
+  completedRegistrationForm: FormGroup;
+  currentRole!: string;
+  addressForm!: FormGroup; // address form
+  // image
+  selectedUserImage!: File;// image
+  retrievedImage: any;
+  viewedImage: any = "/assets/auth/user.png" // default image
+  temporaryRetrievedImage: any;
+  // vahicule
+  maxVehicleNumber: number = 5; // 
+  currentVehicleNumber: number = 1; // user must have at least 1 vehicule
+  vehiculeValues: Vehicle[] = []; //user can have several vehicules, we put them in a list: vehiculeValues
+
+  constructor(private keycloakService: KeycloakService, private authService: AuthService, private userService: UserService, private fb: FormBuilder) { 
+    this.completedRegistrationForm = this.fb.group({
+      img: ['']
+    })
+  }
   ngOnInit(): void {
+    this.currentRole = this.authService.getRoles()[0]; // role is CLIENT by default
+    this.onAddVehicleBtn(); //by default, there is only one input of vehiculeValues
   }
 
   onSubmit(): void {
@@ -31,5 +52,71 @@ export class CompleteRegistrationComponent implements OnInit {
       })
     }
     
+  }
+
+   // Image
+   public onFileChanged(event: any) {  //Gets called when the user selects an image
+    //Select File
+    this.selectedUserImage = event.target.files[0];
+    console.log(this.selectedUserImage)
+    var reader = new FileReader();
+    reader.readAsDataURL(this.selectedUserImage);
+    reader.onload = (_event) => {
+      this.viewedImage = reader.result;
+    }
+    this.completedRegistrationForm.controls['img'].setValue(this.selectedUserImage);
+  }
+
+  //Gets called when the user clicks on save to upload the image
+  onUpload(userId: number) {
+    if (this.completedRegistrationForm.controls['img'].value != undefined) { // if we change the image
+      //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+      const uploadImageData = new FormData();
+      uploadImageData.append('imageFile', this.completedRegistrationForm.controls['img'].value);
+      //uploadImageData.append('profileId', (this.profile.profileId).toString());
+      this.userService.uploadImage(userId, uploadImageData).subscribe(response => { // get api
+        this.viewedImage = this.temporaryRetrievedImage; // view the new image
+      });
+    }
+  }
+
+  getAddressForm(addressForm: FormGroup) {
+    this.addressForm = addressForm;
+    if (this.addressForm.valid) {
+      // save to entity !!! depending on role
+    }
+  }
+
+  onAddVehicleBtn() { // add a new vehicule
+    if (this.vehiculeValues.length < this.maxVehicleNumber) {
+      this.vehiculeValues.push(new Vehicle());
+      this.currentVehicleNumber += 1;
+      console.log(this.vehiculeValues);
+    }
+  }
+
+  onDeleteButton(i: any): void {  //delete the i input field for vehiculesValues list
+    if (this.vehiculeValues.length == 1)   //always keep a field
+      this.vehiculeValues[i] = new Vehicle()
+    else {
+      this.vehiculeValues.splice(i, 1);
+      this.currentVehicleNumber -= 1;
+    }
+  }
+
+  getVehicleForm(vehicleForm: FormGroup, index: number) {
+    console.log(index);
+    console.log(vehicleForm);
+    this.vehiculeValues[index].typeImmat = vehicleForm.controls['registrationType'].value;
+    this.vehiculeValues[index].numImmat = vehicleForm.controls['registrationNumber'].value;
+    this.vehiculeValues[index].confirmNumImmat = vehicleForm.controls['confirmRegistrationNumber'].value;
+    this.vehiculeValues[index].marque = vehicleForm.controls['brand'].value;
+    this.vehiculeValues[index].numChassis = vehicleForm.controls['chassisNumber'].value;
+    this.vehiculeValues[index].numContrat = vehicleForm.controls['contractNumber'].value;
+    this.vehiculeValues[index].couleur = vehicleForm.controls['color'].value;
+    this.vehiculeValues[index].kilometrage = vehicleForm.controls['kilometrage'].value;
+    this.vehiculeValues[index].puissance = vehicleForm.controls['power'].value;
+    this.vehiculeValues[index].nbPortes = vehicleForm.controls['doorsNumber'].value;
+    this.vehiculeValues[index].poids = vehicleForm.controls['weight'].value;
   }
 }
