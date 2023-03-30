1acd7b8b24841eed418f60dc2ecae409463769fe
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:23:11 +0200

    requestMappng

--- assurance-microservice/src/main/java/com/tunidesign/assurancemicroservice/web/AssuranceController.java
+++ assurance-microservice/src/main/java/com/tunidesign/assurancemicroservice/web/AssuranceController.java
@@ -9,35 +9,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/assurance")
 public class AssuranceController {
     @Autowired
     private AssuranceRepository assuranceRepository;
-    @GetMapping("/Assurances")
+    @GetMapping("/getAll")
     public List<Assurance> listeAssurances()
     {
         return assuranceRepository.findAll();
     }
 
-    @GetMapping(value = "/Assurances/{id}")
+    @GetMapping(value = "/getById/{id}")
     public Assurance afficherUnAssurance(@PathVariable int id) {
         Assurance assurance = assuranceRepository.findById(id);
         if(assurance==null) throw new AssuranceIntrouvableException("L'Assurance' avec l'id " + id + " est INTROUVABLE. ");
         return assurance;
     }
 
-    @PostMapping(value = "/PasserUneAssurance")
+    @PostMapping(value = "/add")
     public void passerUnAssurance(@RequestBody Assurance assurance)
     {
         assuranceRepository.save(assurance);
     }
-    @DeleteMapping (value = "/AnnulerUnAssurance/{id}")
+    @DeleteMapping (value = "/delete/{id}")
     public void supprimerUnAssurance(@PathVariable int id)
     {
         Assurance assurance = assuranceRepository.deleteById(id);
         if(assurance==null) throw new AssuranceIntrouvableException("L'Assuranceavec l'id " + id + " est INTROUVABLE. ");
 
     }
-    @PutMapping (value = "/ModifierUneAssurance")
+    @PutMapping (value = "/update")
     public void modiferUnAssurance(@RequestBody Assurance assurance)
     {
         assuranceRepository.save(assurance);
