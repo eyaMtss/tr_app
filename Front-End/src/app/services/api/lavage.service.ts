4fff0c1e39796c60c17fb9c16672cdd9fd652ae7
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:27:22 +0200

    lavage service

---
+++ Front-End/src/app/services/api/lavage.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Lavage } from 'src/app/models/lavage';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LavageService {
+  private _URL = environment.domain + ":" + environment.port + "/lavage/";
+  constructor(private httpClient: HttpClient) { }
+
+  create(lavage: Lavage): Observable<Lavage> {
+    return this.httpClient.post<Lavage>(this._URL + "add", lavage);
+  }
+}
