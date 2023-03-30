b131e22c2d8b4776d949c94b762a72cb0bf37cc8
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 15:24:16 +0200

    mapper (class name)

---
+++ garage-microservice/src/main/java/com/tunidesign/garagemicroservice/mapper/GarageMapperImpl.java
@@ -0,0 +1,28 @@
+package com.tunidesign.garagemicroservice.mapper;
+
+
+import com.tunidesign.garagemicroservice.DTO.GarageRequestDTO;
+import com.tunidesign.garagemicroservice.DTO.GarageResponseDTO;
+import com.tunidesign.garagemicroservice.model.Garage;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GarageMapperImpl implements GarageMapper{
+    @Override
+    public GarageResponseDTO garageToGarageResponseDTO(Garage garage) {
+       return  GarageResponseDTO.builder().id(garage.getId())
+                .capacite(garage.getCapacite())
+                .adresse(garage.getAdresse()).build();
+
+    }
+
+    @Override
+    public Garage garageRequestDTOToGarage(GarageRequestDTO garageRequestDTO) {
+      return  Garage.builder().id(garageRequestDTO.getId())
+               .capacite(garageRequestDTO.getCapacite())
+               .adresse(garageRequestDTO.getAdresse()).build();
+
+
+    }
+}
