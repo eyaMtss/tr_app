1c18e5c26c8efc71eec8b02539555a8650cc1e74
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 15:23:43 +0200

    add-lavage

---
+++ Front-End/src/app/shared/add-lavage/add-lavage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-lavage',
+  templateUrl: './add-lavage.component.html',
+  styleUrls: ['./add-lavage.component.css']
+})
+export class AddLavageComponent {
+
+}
