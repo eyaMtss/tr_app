183ff6a6e64d383e8c4f5ef58b25f6c4f6e46728
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:26:50 +0200

    garage model

---
+++ Front-End/src/app/models/garage.ts
@@ -0,0 +1,22 @@
+export class Garage {
+    name!: string;
+    capacity!: number;
+    garageOwner!: number;
+    email!: string;
+    phone!: number;
+    address!: string;
+    garageType!: string;
+
+    public constructor();
+    public constructor(name: string, capacity: number, garageOwner: number, email: string, 
+        phone: number, address: string);
+    public constructor(...myarray: any[]){
+        this.name = myarray[0];
+        this.capacity = myarray[1];
+        this.email = myarray[2];
+        this.garageOwner = myarray[3];
+        this.email = myarray[4];
+        this.phone = myarray[5];
+        this.address = myarray[6];
+    }
+}
