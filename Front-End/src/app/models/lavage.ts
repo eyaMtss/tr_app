e9de7ae1cdb2e4177127f424f9622ce445642f09
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:27:01 +0200

    lavage model

---
+++ Front-End/src/app/models/lavage.ts
@@ -0,0 +1,23 @@
+export class Lavage {
+    name!: string;
+    capacity!: number;
+    currentNbVehicle!: number;
+    lavageOwner!: number;
+    email!: string;
+    phone!: number;
+    address!: string;
+
+    public constructor();
+    public constructor(name: string, capacity: number, currentNbVehicle: number, lavageOwner: number,
+        email: string, phone: number, address: string);
+    public constructor(...myarray: any[]){
+        this.name = myarray[0];
+        this.capacity = myarray[1];
+        this.email = myarray[2];
+        this.currentNbVehicle = myarray[3];
+        this.lavageOwner = myarray[4];
+        this.email = myarray[5];
+        this.phone = myarray[6];
+        this.address = myarray[7];
+    }
+}
