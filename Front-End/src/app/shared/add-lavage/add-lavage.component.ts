a263c4832a5e74bcb01c135111d265865f52d4e6
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:25:11 +0200

    add lavage c

--- Front-End/src/app/shared/add-lavage/add-lavage.component.ts
+++ Front-End/src/app/shared/add-lavage/add-lavage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-lavage',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-lavage.component.css']
 })
 export class AddLavageComponent {
+  lavageForm: FormGroup;
+  // lavage 
+  lavageType: string = "";
+  lavageTypeValue!: string; // ngModel
+  @Output() lavageFormEvent = new EventEmitter<FormGroup>();
+  constructor(private _formBuilder: FormBuilder){
+    this.lavageForm = this._formBuilder.group({
+      name: ["", Validators.required],
+      capacity: ["", Validators.required],
+      lavageOwner: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", Validators.required],
+      address: ["", Validators.required]
+    });
+  }
 
+  onFormChange(){
+    if(this.lavageForm.valid){
+        this.emitInformationForm(this.lavageForm);
+      } 
+  }
+
+  emitInformationForm(value: FormGroup) {
+    this.lavageFormEvent.emit(value);
+  }
 }
