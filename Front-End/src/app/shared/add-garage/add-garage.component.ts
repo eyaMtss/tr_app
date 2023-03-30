dc743c1433d1e233a00de37582465c3842333c09
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 15:23:29 +0200

    add-garage

---
+++ Front-End/src/app/shared/add-garage/add-garage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-garage',
+  templateUrl: './add-garage.component.html',
+  styleUrls: ['./add-garage.component.css']
+})
+export class AddGarageComponent {
+
+}
