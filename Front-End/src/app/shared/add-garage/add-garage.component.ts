02ab7735d41453301cfce7704b6df8b5667d9e29
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-30 17:24:40 +0200

    add garage  c

--- Front-End/src/app/shared/add-garage/add-garage.component.ts
+++ Front-End/src/app/shared/add-garage/add-garage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-garage',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-garage.component.css']
 })
 export class AddGarageComponent {
+  garageForm: FormGroup;
+  // garage 
+  garageType: string = "";
+  garageTypeValue!: string; // ngModel
+  @Output() garageFormEvent = new EventEmitter<FormGroup>();
+  constructor(private _formBuilder: FormBuilder){
+    this.garageForm = this._formBuilder.group({
+      name: ["", Validators.required],
+      capacity: ["", Validators.required],
+      garageOwner: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", Validators.required],
+      address: ["", Validators.required],
+      garageType: ["", Validators.required]
+    });
+  }
 
+  onGarageTypeChange(){
+    let garageTypeValue = this.garageForm.controls['garageType'].value;
+    console.log(garageTypeValue)
+    console.log(garageTypeValue.includes("wrench"))
+    if(garageTypeValue.includes("wrench")){
+      this.garageType = "mecanique";
+    }
+    else if(garageTypeValue.includes("plug")){
+      this.garageType = "electrique";
+    }
+    else if(garageTypeValue.includes("tire")){
+      this.garageType = "pneumatique";
+    }
+    console.log(this.garageType);
+  }
+
+  onFormChange(){
+    if(this.garageForm.valid){
+        this.emitInformationForm(this.garageForm);
+      } 
+  }
+
+  emitInformationForm(value: FormGroup) {
+    this.garageFormEvent.emit(value);
+  }
 }
