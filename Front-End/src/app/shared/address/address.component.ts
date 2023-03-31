155f8bf27efacbd93206459fb9d0b2c0064e47ed
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-31 02:47:09 +0200

    address c

--- Front-End/src/app/shared/address/address.component.ts
+++ Front-End/src/app/shared/address/address.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -8,91 +9,62 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddressComponent implements OnInit {
   addressForm: FormGroup;
-  countries = [{ id: "TUN", value: "Tunisie" }, { id: "ALG", value: "Algerie" }, { id: "LYB", value: "Lybia" }];
-  isCountrySetted: boolean = false;
-  governorates: any[] = [];
-  isGovernorateSetted: boolean = false;
-  cities: any[] = [];
+  country: any
+  governorates: any;
+  citiesJSON: any;
+  cities!: any;
+  countries = [{ id: "TUN", value: "Tunisie" }, { id: "LYB", value: "Libye" }, { id: "ALG", value: "Algérie" }, { id: "FR", value: "France" }];
+
 
   @Output() addressFormEvent = new EventEmitter<FormGroup>();
-  
-  constructor(private _formBuilder: FormBuilder){
+
+
+  constructor(private _formBuilder: FormBuilder, private httpClient: HttpClient) {
     this.addressForm = this._formBuilder.group({
-      country: ["", Validators.required],
+      country: ["TUN", Validators.required],
       governorate: ["", Validators.required],
       city: ["", Validators.required],
       zipCode: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
-      //homeCode: [{value: "", disabled: true}, Validators.required],
+      homeCode: ["", Validators.required],
     });
   }
 
   ngOnInit(): void {
     this.addressForm.controls['country'].setValue("TUN"); // country is setted to Tunisia
-    this.onCountryChange(); // get Tunisia's governorates
   }
 
-  onCountryChange() { // get governorates depending on the selected country
-    this.isCountrySetted = true;
-    this.getGovernorates(this.addressForm.controls['country'].value);
+
+  onCountryChange(e: any) {
+    this.addressForm.controls['country'].setValue(e.target.value);
+    this.httpClient.get('assets/address/gouv.json').subscribe(data => {
+      console.log(data);
+      this.governorates = data
+    });
   }
 
-  onGovernorateChange() { // get cities depending on the selected country&&governorate
-    this.isGovernorateSetted = true;
-    this.getCities(this.addressForm.controls['country'].value, this.addressForm.controls['governorate'].value);
+  onGovernorateChange(e: any) {
+    let governorate = e.target.value;
+    this.addressForm.controls['governorate'].setValue(governorate);
+
+    this.httpClient.get('assets/address/data.json').subscribe((data: any) => {
+      console.log(data);
+      this.citiesJSON = Object.entries(data);
+      let currentGovernorateName = this.governorates.filter((gov:any) => gov.id == governorate)[0].name;
+      this.cities = (this.citiesJSON.filter((e: any) => e[0] == currentGovernorateName)[0])[1];
+    });
   }
 
-  getGovernorates(country: string) {
-    let governorate: string[] = [];
-    let id: number = 0;
-    switch (country) {
-      case "TUN": {
-        this.governorates.splice(0);
-        governorate = ["Ariana", "Béja", "Ben Arous", "Bizerte", "Gabès", "Gafsa", "Jendouba", "Kairouan",
-          "Kasserine", "Kebili", "Kef", "Mahdia", "Manouba", "Medenine", "Monastir", "Nabeul", "Sfax",
-          "Sidi Bouzid", "Siliana", "Sousse", "Tataouine", "Tozeur", "Tunis", "Zaghouan"];
-        break;
-      }
-      case "ALG": {
-        this.governorates.splice(0);
-        governorate = ["Adrar", "Chlef", "Laghouat", "Oum El Bouaghi", "Batna", "Béjaïa", "Biskra", "Béchar",
-          "Blida", "Bouira", "Tamanrasset", "Tébessa", "Tlemcen", "Tiaret", "Tizi Ouzou", "Alger", "Djelfa",
-          "Jijel", "Sétif", "Saïda", "Skikda", "Sidi Bel Abbès", "Annaba", "Guelma", "Constantine", "Médéa",
-          "Mostaganem", "Mascara", "Ouargla", "Oran", "M'Sila", "El Bayadh", "Illizi", "Bordj Bou Arreridj",
-          "Boumerdès", "El Tarf", "Tindouf", "Tissemsilt", "El Oued", "Khenchela", "Souk Ahras", "Tipaza",
-          "Mila", "Aïn Defla", "Naâma", "Aïn Témouchent", "Ghardaïa", "Relizane"];
-        break;
-      }
-      /*case "LYB": {
-        this.governorates.splice(0);
-        governorate = [];
-        break;
-      }*/
-    }
-    this.governorates = governorate;
-    //governorate.forEach(e => this.governorates.push({ id: id + 1, value: e }));
+  onCityChange(e: any) {
+    let city = e.target.value;
+    this.addressForm.controls['city'].setValue(city);
+    let zipCode = this.cities.filter((e: any) => e.ville == city)[0].code_postal;
+    this.addressForm.controls['zipCode'].setValue(zipCode);
   }
 
-  getCities(country: string, governorate: string) {
-    let cities: string[] = [];
-    let id: number = 0;
-    console.log(country)
-    switch (country) {
-      case "TUN": { // Tunisia
-        console.log(governorate)
-        if(governorate == "Ariana"){
-          cities = ["Ariana", "Sokra", "ghazela"];
-        }
-        break;
-      }
-    }
-    this.cities = cities;
-    //cities.forEach(e => this.cities.push({ id: id + 1, value: e }));
-  }
-
-  onFormChange(){
-    if(this.addressForm.valid){
+  onFormChange() {
+    if (this.addressForm.valid) {
       this.emitInformationForm(this.addressForm);
-    } 
+    }
   }
 
   emitInformationForm(value: FormGroup) {
