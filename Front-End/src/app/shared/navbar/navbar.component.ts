816037a5eac9e74d9f6054a6fcc44bd2648c8b4f
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-02 19:49:08 +0200

    cleanup

--- Front-End/src/app/shared/navbar/navbar.component.ts
+++ Front-End/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { KeycloakLoginOptions } from 'keycloak-js';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -8,26 +8,35 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit, OnChanges{
   @Input() isLoggedIn!: boolean; // 
   username: string = "";
+  currentRole: string = "";
   constructor(private router: Router, private authService: AuthService) {
     
   }
-  ngOnInit(): void {
-    if (this.authService.getIsLoggedIn()){
-      this.isLoggedIn = true;
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.isLoggedIn)
+    if (this.isLoggedIn == true){
       this.username = this.authService.getUsername();
+      this.currentRole = this.authService.getRoles()[0];
+      console.log(this.currentRole)
     }
-      
-    this.isLoggedIn = false;
-    localStorage.setItem("isLoggedIn", this.isLoggedIn.toString()); // set isLoggedIn attribute in LocalStorage
-
-    
+    localStorage.setItem("isLoggedIn", this.isLoggedIn? "true":"false"); // set isLoggedIn attribute in LocalStorage
+  }
+  ngOnInit(): void {
   }
 
   home() {
-    this.router.navigate(['/']);
+    console.log(this.username)
+    console.log(this.currentRole)
+    if(this.currentRole == "CLIENT") this.router.navigate(["/client"]);
+    else if(this.currentRole == "GARAGISTE_ADMIN") this.router.navigate(["/garagisteAdmin"]);
+    else if(this.currentRole == "LAVAGISTE_ADMIN") this.router.navigate(["/lavagisteAdmin"]);
+    else if(this.currentRole == "INSURANCE_ADMIN") this.router.navigate(["/insuranceAdmin"]);
+    else if(this.currentRole == "AGENCE_LOCATION_ADMIN") this.router.navigate(["/agenceLocationAdmin"]);
+    else if(this.currentRole == "DRIVER") this.router.navigate(['camion-login']);
+    else this.router.navigate(["/"]);
   }
 
   signin() {
@@ -47,7 +56,7 @@ export class NavbarComponent implements OnInit{
   }
 
   profile(){
-    
+    if(this.currentRole == "CLIENT") this.router.navigate(["/client/profile"]);
   }
 
   orders(){
