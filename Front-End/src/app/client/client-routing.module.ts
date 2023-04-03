4e666df82e3ca9812b8c2571fe97813d36cb77dd
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-03 16:06:35 +0200

    add profile path

--- Front-End/src/app/client/client-routing.module.ts
+++ Front-End/src/app/client/client-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client.component';
 import { ShareInformationComponent } from './share-information/share-information.component';
 import { SendPictureComponent } from './send-picture/send-picture.component';
+import { ProfileClientComponent } from './profile-client/profile-client.component';
 
 const routes: Routes = [
   { path: '', component: ClientComponent },
   { path: 'share', component: ShareInformationComponent },
   { path: 'picture', component: SendPictureComponent },
+  { path: 'profile', component: ProfileClientComponent },
 ];
 
 @NgModule({
