0d043da3f27f2efa88f61fe4fbc84495e7f253cc
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-04 00:11:47 +0200

    add-office(expert)

---
+++ Front-End/src/app/shared/components/add-office/add-office.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-office',
+  templateUrl: './add-office.component.html',
+  styleUrls: ['./add-office.component.css']
+})
+export class AddOfficeComponent {
+
+}
