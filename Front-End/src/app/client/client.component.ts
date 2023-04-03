aabd688955d901409d88b29d2f2cec1272bcbb11
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-04 00:03:09 +0200

    add constat service

--- Front-End/src/app/client/client.component.ts
+++ Front-End/src/app/client/client.component.ts
@@ -9,5 +9,7 @@ export class ClientComponent {
 
   services: any[] = [
     {id: "remorquage", name: "Remorquage", image: "/assets/client-services/remorquage.png", btn_label: "Passer un ordre", link: "/client/share"},
+    {id: "constat", name: "Constat numérique", image: "/assets/client-services/constat.png", btn_label: "Passer un constat", link: "/client/share"},
+  
   ]
 }
