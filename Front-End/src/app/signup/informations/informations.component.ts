ba31ed8dfa863f6925f03612d8a541042df992bb
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-04 00:07:03 +0200

    agence location

--- Front-End/src/app/signup/informations/informations.component.ts
+++ Front-End/src/app/signup/informations/informations.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountryISO, SearchCountryField } from 'ngx-intl-tel-input';
 import { Company } from 'src/app/models/company';
+import { AgenceLocationService } from 'src/app/services/api/agence-location.service';
 import { InsuranceService } from 'src/app/services/api/insurance.service';
 import { SocieteRemorquageService } from 'src/app/services/api/societe-remorquage.service';
 
@@ -14,19 +16,22 @@ export class InformationsComponent implements OnInit {
   informationForm: FormGroup;
   selectedUserImage!: File;// image
   viewedImage: any = "/assets/auth/user.png" // default image
-  roles: any[] = [{id: "CLIENT", value: "Client"}, {id: "INSURANCE_ADMIN", value: "Assurance"}, 
-  {id: "SOCIETE_REMORQUAGE_ADMIN", value: "Société de remorquage" },
-  {id: "GARAGISTE_ADMIN", value: "Garagiste"}, {id: "AGENCE_LOCATION_ADMIN", value: "Agence de location"}, 
-  {id: "LAVAGISTE_ADMIN", value: "Lavagiste"}, {id: "EXPERT", value: "Expert"}]; //role values
+  roles: any[] = [{ id: "CLIENT", value: "Client" }, { id: "INSURANCE_ADMIN", value: "Assurance" },
+  { id: "SOCIETE_REMORQUAGE_ADMIN", value: "Société de remorquage" },
+  { id: "GARAGISTE_ADMIN", value: "Garagiste" }, { id: "AGENCE_LOCATION_ADMIN", value: "Agence de location" },
+  { id: "LAVAGISTE_ADMIN", value: "Lavagiste" }, { id: "EXPERT", value: "Expert" }]; //role values
   currentRole: string = "CLIENT"; // default role = user
   companyList: Company[] = []; // company(insurance / société remorquage)
   companyLabel!: string;
 
-  genreList = [{id: "F", value: "Féminin"}, {id: "M", value: "Masculin"}];
+  genderList = [{ id: "F", value: "Féminin" }, { id: "M", value: "Masculin" }];
   @Output() informationsFormEvent = new EventEmitter<FormGroup>();
   @Output() currentRoleEvent = new EventEmitter<string>();
+
+  CountryISO = CountryISO;
+  SearchCountryField = SearchCountryField;
   constructor(private _formBuilder: FormBuilder, private societeRemorquageService: SocieteRemorquageService,
-    private insuranceService: InsuranceService) {
+    private insuranceService: InsuranceService, private agenceLocationService: AgenceLocationService) {
     this.informationForm = this._formBuilder.group({
       role: ["CLIENT", Validators.required],
       firstname: ["", Validators.required],
@@ -43,19 +48,6 @@ export class InformationsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // Image
-  public onFileChanged(event: any) {  //Gets called when the user selects an image
-    //Select File
-    this.selectedUserImage = event.target.files[0];
-    console.log(this.selectedUserImage)
-    var reader = new FileReader();
-    reader.readAsDataURL(this.selectedUserImage);
-    reader.onload = (_event) => {
-      this.viewedImage = reader.result;
-    }
-    this.informationForm.controls['img'].setValue(this.selectedUserImage);
-  }
-
   onRoleChange() {
 
     this.currentRole = this.informationForm.controls['role'].value;
@@ -65,22 +57,26 @@ export class InformationsComponent implements OnInit {
     switch (this.currentRole) {
       case "INSURANCE_ADMIN": { //assurance
         this.informationForm.controls['company'].enable();
+        this.informationForm.controls['username'].disable();
         this.getAllInsurances();
         break;
       }
       case "SOCIETE_REMORQUAGE_ADMIN": { //société de remorquage
         console.log(this.currentRole);
         this.informationForm.controls['company'].enable();
+        this.informationForm.controls['username'].disable();
         this.getAllSocietesRemorquage();
         break;
       }
       case "AGENCE_LOCATION_ADMIN": { // agence de location
         this.informationForm.controls['company'].enable();
+        this.informationForm.controls['username'].disable();
         //this.getAllAgencesLocation();
         break;
       }
-      default:{
+      default: {
         this.informationForm.controls['company'].disable();
+        this.informationForm.controls['username'].enable();
         break;
       }
     }
@@ -102,15 +98,23 @@ export class InformationsComponent implements OnInit {
     })
   }
 
+  getAllRentAgencies() {
+    this.agenceLocationService.getAll().subscribe(data => {
+      console.log(data);
+      this.companyList.splice(0);
+      this.companyList = data;
+    })
+  }
+
   onClick() {
     console.log(this.informationForm.valid);
   }
 
-  onFormChange(){
+  onFormChange() {
     //this.informationForm.valueChanges.subscribe(val => {
-      if(this.informationForm.valid){
-        this.emitInformationForm(this.informationForm);
-      }
+    if (this.informationForm.valid) {
+      this.emitInformationForm(this.informationForm);
+    }
     //});  
   }
 
@@ -119,7 +123,7 @@ export class InformationsComponent implements OnInit {
     this.informationsFormEvent.emit(value);
   }
 
-  emitCurrentrole(value: string){
+  emitCurrentrole(value: string) {
     this.currentRoleEvent.emit(value);
   }
 }
