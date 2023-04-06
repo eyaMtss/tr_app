733288ec38af4e73edcd7127126901ebf6c901d7
Sirine Ferchichi <sirine.ferchichi@tunidesign.net>
2023-04-06 14:21:32 +0000

    ajout

--- utilisateur-microservice/src/main/java/com/tunidesign/utilisateurmicroservice/model/enumeration/Role.java
+++
@@ -1,6 +0,0 @@
-package com.tunidesign.utilisateurmicroservice.model.enumeration;
-
-public enum Role {
-	CLIENT, SUPER_ADMIN, SOCIETE_REMORQUAGE_ADMIN, TA, DRIVER, AGENCY_ADMIN, INSURANCE_ADMIN,
-	EXPERT, AGENCE_LOCATION_ADMIN, GARAGISTE_ADMIN, LAVAGISTE_ADMIN
-}
