904fbb49cdb47c13f5bacd65ec1982410b3722d5
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-10 02:18:00 +0200

    requestMapping

--- recu-microservice/src/main/java/com/tunidesign/recumicroservice/web/RecuController.java
+++ recu-microservice/src/main/java/com/tunidesign/recumicroservice/web/RecuController.java
@@ -8,36 +8,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@RequestMapping("/recu")
 public class RecuController {
     @Autowired
     private RecuRepository recuRepository;
-    @GetMapping("/Recus")
+    @GetMapping("/getAll")
     public List<Recu > listeRecus()
     {
         return recuRepository.findAll();
     }
 
-    @GetMapping(value = "/Recus/{id}")
-    public Recu afficherUnRecu(@PathVariable int id) {
-        return  recuRepository.findById(id);
+    @GetMapping(value = "/getById/{id}")
+    public Recu afficherUnRecu(@PathVariable Long id) {
+        return  recuRepository.findById(id).get();
         //if(service==null) throw new RecuIntrouvableException("Le service avec l'id " + id + " est INTROUVABLE. ");
        // return service;
     }
 
-    @PostMapping(value = "/Recus/AjouterUnRecu")
+    @PostMapping(value = "/add")
     public void ajouterUnrecu(@RequestBody Recu recu)
     {
         recuRepository.save(recu);
     }
-    @DeleteMapping (value = "/Recus/SupprimerUnRecu/{id}")
-    public void supprimerUnRecu(@PathVariable int id)
+    @DeleteMapping (value = "/delete/{id}")
+    public void supprimerUnRecu(@PathVariable Long id)
     {
         recuRepository.deleteById(id);
         // if(facture==null) throw new RecuIntrouvableException("La facture avec l'id " + id + " est INTROUVABLE. ");
 
     }
-    @PutMapping (value = "/Recu/ModifierUnRecu")
+    @PutMapping (value = "/update")
     public void modiferUnRecu(@RequestBody Recu recu)
     {
            recuRepository.save(recu);
