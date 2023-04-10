865e7f3e56aab10b56155808c57fbe7acb1446a3
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-10 02:25:54 +0200

    requestMapping

--- service-microservice/src/main/java/com/tunidesign/servicemicroservice/web/ServiceController.java
+++ service-microservice/src/main/java/com/tunidesign/servicemicroservice/web/ServiceController.java
@@ -6,36 +6,36 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@CrossOrigin
+@RequestMapping("/service")
 public class ServiceController {
     @Autowired
     private ServiceRepository serviceRepository;
-    @GetMapping("/Services")
+    @GetMapping("/getAll")
     public List<Service > listeServices()
     {
         return serviceRepository.findAll();
     }
 
-    @GetMapping(value = "/Services/{id}")
+    @GetMapping(value = "/getById/{id}")
     public Service afficherUnService(@PathVariable Long id) {
         return  serviceRepository.findById(id).get();
         //if(service==null) throw new ServiceIntrouvableException("Le service avec l'id " + id + " est INTROUVABLE. ");
        // return service;
     }
 
-    @PostMapping(value = "/Services/AjouterUnService")
+    @PostMapping(value = "/add")
     public void ajouterUnservice(@RequestBody Service service)
     {
         serviceRepository.save(service);
     }
-    @DeleteMapping (value = "/Services/SupprimerUnService/{id}")
+    @DeleteMapping (value = "/delete/{id}")
     public void supprimerUnService(@PathVariable Long id)
     {
         serviceRepository.deleteById(id);
         // if(facture==null) throw new FactureIntrouvableException("La facture avec l'id " + id + " est INTROUVABLE. ");
 
     }
-    @PutMapping (value = "/Services/ModifierUnService")
+    @PutMapping (value = "/update")
     public void modiferUnService (@RequestBody Service service)
     {
            serviceRepository.save(service);
