180a1cab1bda9b74c339a36fd21b634a899691cf
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-10 02:33:02 +0200

    requestMapping

--- shift-microservice/src/main/java/com/tunidesign/shiftmicroservice/web/ShiftController.java
+++ shift-microservice/src/main/java/com/tunidesign/shiftmicroservice/web/ShiftController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/shift")
 public class ShiftController {
     @Autowired
     private ShiftService shiftService;
@@ -20,27 +21,27 @@ public class ShiftController {
     public String test() {
         return "ça marche";
     }
-    @GetMapping("/shifts")
+    @GetMapping("/getAll")
     public List<ShiftResponseDTO> listeShifts() {
         return shiftService.getShifts();
     }
 
-    @GetMapping(value = "/shifts/{id}")
+    @GetMapping(value = "/getById/{id}")
     public ShiftResponseDTO afficherUnShift(@PathVariable Long id) throws ShiftIntrouvableException {
         ShiftResponseDTO shift = shiftService.getShiftById(id);
         if(shift==null) throw new ShiftIntrouvableException("Le shift avec l'id " + id + " est INTROUVABLE. ");
         return shift;
     }
-    @PostMapping(value = "/SaveShift")
+    @PostMapping(value = "/add")
     public void saveShift(@RequestBody ShiftRequestDTO shiftRequestDTO) {
         shiftService.save(shiftRequestDTO);
     }
-    @DeleteMapping (value = "/DeleteShift/{id}")
+    @DeleteMapping (value = "/delete/{id}")
     public void supprimerUnShift(@PathVariable Long id)
     {
         shiftService.deleteShift(id);
     }
-    @PutMapping (value = "/ModifierUnShift")
+    @PutMapping (value = "/update")
     public ShiftResponseDTO modiferUnShift(@RequestBody ShiftRequestDTO shiftRequestDTO) throws ShiftIntrouvableException {
         ShiftResponseDTO shift = shiftService.save(shiftRequestDTO);
         if(shift==null) throw new ShiftIntrouvableException("Ce shift est INTROUVABLE. ");
