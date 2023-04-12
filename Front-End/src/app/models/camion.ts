f91a6c16886688a7d9a829a0ec84d5ca17f75ca1
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-12 06:35:22 +0200

    attr

--- Front-End/src/app/models/camion.ts
+++ Front-End/src/app/models/camion.ts
@@ -1,9 +1,18 @@
 export class Camion {
+	id!: number
+	matricule!: string;
+	modele!: string;
+	charge!: number;
+	poids!: number;
+	type!: string;
+
 	porte!: number;
 	typeImmat!: string;
 	typePoidsLourd!: string;
 	serie!: number;
 	numImmat!: number;
 	numChassis!: number;
-	companyID!: number
+	companyID!: number;
+	positionAtt!: number;
+	positionLong!: number;
 }
