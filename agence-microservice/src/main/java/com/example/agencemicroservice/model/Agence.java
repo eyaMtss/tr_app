afa21baa504721c6afee80ac37a56db11d6d072c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-12 05:43:24 +0200

    add attr

--- agence-microservice/src/main/java/com/example/agencemicroservice/model/Agence.java
+++ agence-microservice/src/main/java/com/example/agencemicroservice/model/Agence.java
@@ -2,20 +2,25 @@ package com.example.agencemicroservice.model;
 
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "agence")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Agence {
+    @Transient
+    public static final String SEQUENCE_NAME = "agence_sequence";
     @Id
     private Long id;
     private int numero ;
-    private String nom ;
+    private String name;
     private int telephone ;
     private String responsable ;
     @Email
