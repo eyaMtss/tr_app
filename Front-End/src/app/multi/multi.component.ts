4371aa2c9bb84d972710714d1be2000290c22db1
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-12 09:28:15 +0200

    add routes

--- Front-End/src/app/multi/multi.component.ts
+++ Front-End/src/app/multi/multi.component.ts
@@ -19,8 +19,10 @@ export class MultiComponent implements OnInit {
     else if(role == "INSURANCE_ADMIN") this.router.navigate(["/insuranceAdmin"]);
     else if(role == "AGENCE_LOCATION_ADMIN") this.router.navigate(["/agenceLocationAdmin"]);
     else if(role == "DRIVER") this.router.navigate(['camion-login']);
+    else if(role == "TA") this.router.navigate(["/ta"]);
+    else if(role == "SOCIETE_REMORQUAGE_ADMIN") this.router.navigate(["/societe"]);
     else if(role == "EXPERT") this.router.navigate(["/expert"]);
-    else this.router.navigate(["/order"]);
+    else this.router.navigate(["/home"]);
   }
 
 }
