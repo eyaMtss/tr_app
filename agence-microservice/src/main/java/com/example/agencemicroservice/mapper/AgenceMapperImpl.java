7e99534407f8ecd712747e3c3d90fe68c9222f23
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-12 05:43:09 +0200

    cleanup

--- agence-microservice/src/main/java/com/example/agencemicroservice/mapper/AgenceMapperImpl.java
+++ agence-microservice/src/main/java/com/example/agencemicroservice/mapper/AgenceMapperImpl.java
@@ -3,17 +3,44 @@ package com.example.agencemicroservice.mapper;
 import com.example.agencemicroservice.DTO.AgenceRequestDTO;
 import com.example.agencemicroservice.DTO.AgenceResponseDTO;
 import com.example.agencemicroservice.model.Agence;
+import com.example.agencemicroservice.service.SequenceGeneratorService;
+import jakarta.validation.constraints.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AgenceMapperImpl implements AgenceMapper {
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
     @Override
     public AgenceResponseDTO agenceToAgenceDTO(Agence agence) {
-        return null;
+        return AgenceResponseDTO.builder()
+                .id(agence.getId())
+                .numero(agence.getNumero())
+                .name(agence.getName())
+                .telephone(agence.getTelephone())
+                .responsable(agence.getResponsable())
+                .email(agence.getEmail())
+                .ville(agence.getVille())
+                .paye(agence.getPaye())
+                .gouvernerat(agence.getGouvernerat())
+                .idAssurance(agence.getIdAssurance())
+                .build();
     }
     @Override
     public Agence agenceRequestDTOToAgence(AgenceRequestDTO agenceRequestDTO) {
-        return null;
+        return Agence.builder()
+                .id(sequenceGeneratorService.generateSequence(Agence.SEQUENCE_NAME))
+                .numero(agenceRequestDTO.getNumero())
+                .name(agenceRequestDTO.getName())
+                .telephone(agenceRequestDTO.getTelephone())
+                .responsable(agenceRequestDTO.getResponsable())
+                .email(agenceRequestDTO.getEmail())
+                .ville(agenceRequestDTO.getVille())
+                .paye(agenceRequestDTO.getPaye())
+                .gouvernerat(agenceRequestDTO.getGouvernerat())
+                .idAssurance(agenceRequestDTO.getIdAssurance())
+                .build();
     }
 
 }
